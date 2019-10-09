47cf68f191c471fdb0de1c2c4404b8692f04092a
yahmlevi <yahmlevi@gmail.com>
2019-10-09 18:34:52 +0300

    Removed imshow

--- lanes.py
+++ lanes.py
@@ -74,7 +74,8 @@ while(cap.isOpened()):
     line_image = display_lines(frame, averaged_lines)
     combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
     # cv2.imshow("result", combo_image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
+
 cap.release()
 cv2.destroyAllWindows()
