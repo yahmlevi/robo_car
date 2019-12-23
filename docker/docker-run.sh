cfe98fd41f048c09137585e3bca5b39e413408a1
Tsadok Levi Firseck <tsadoklf@gmail.com>
2019-12-23 19:20:48 +0200

    Changed base.dockerfile and video-streaner-server.dockerfile

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=$1
+TAG=$1
 
 IMAGE="yahmlevi/opencv"
 # docker run --rm -it --privileged --device=//dev/video0:/dev/video0  $IMAGE:$VERSION
@@ -12,13 +12,14 @@ IMAGE="yahmlevi/opencv"
 # 2nd option
 # https://hub.docker.com/r/sgtwilko/rpi-raspbian-opencv/
 # execute 'xhost +' to allow access from the docker container to the host's display
+function ols(){
 docker run -it --rm \
     -v `pwd`/videos:/videos \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
     --device /dev/video0 \
-    $IMAGE:$VERSION \
-    python ./lanes.py
+    $IMAGE:$TAG
+}
 
 # -v //dev/bus/usb:/dev/bus/usb
 # -v //dev/bus/usb:/dev/bus/usb
@@ -29,3 +30,26 @@ docker run -it --rm \
 
 # Run GUI app in linux docker container on windows host
 # https://dev.to/darksmile92/run-gui-app-in-linux-docker-container-on-windows-host-4kde
+
+
+function run_video_streamer_client(){
+    IMAGE="yahmlevi/robo-car"
+    TAG="video-streamer-client"
+    
+    docker run -it --rm \
+        --device /dev/video0 \
+        $IMAGE:$TAG
+}
+
+function run_video_streamer_server(){
+    IMAGE="yahmlevi/robo-car"
+    TAG="video-streamer-server"
+    
+    docker run -it --rm \
+        -p 5555:5555 \
+        -p 8001:8000 \
+        --name video-streamer-server \
+        $IMAGE:$TAG
+
+    echo "Run the Video Streamer Client on RPI and then goto http://localhost:8001 to see the video stream"    
+}
